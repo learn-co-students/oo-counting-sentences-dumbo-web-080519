1397496a368d553b861578203739aa9641cb92e3
Zermina Ejaz <zerminaejaz@gmail.com>
2019-12-05 17:51:07 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,27 @@ require 'pry'
 
 class String
 
-  def sentence?
+  def sentence?()
+    return self.end_with?(".")
     
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[!.?] /)  # regex
+    if array.length === 0
+      return 0
+    elsif array.length != 1
+      return array.length
+    else
+      return 1
+    end
   end
 end
